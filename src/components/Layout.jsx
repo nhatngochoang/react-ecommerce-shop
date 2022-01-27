cf3f8c1d30cf46a7391e538ca162cab7e571f98b
tahn-0102 <ongthoxunu@gmail.com>
2022-01-28 01:11:23 +0700

    Day 1

---
+++ src/components/Layout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from 'components/Header';
+import Footer from 'components/Footer'
+import Routes from '../routes/Routes'
+
+const Layout = () => {
+   return (
+      <BrowserRouter>
+         <Route render={props => (
+            <div>
+               <Header {...props} />
+               <div className="container">
+                  <div className="main">
+                     <Routes />
+                  </div>
+               </div>
+               <Footer />
+            </div>
+         )} />
+      </BrowserRouter>
+   )
+}
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import 'assets/boxicons-2.0.7/css/boxicons.min.css';
+import 'sass/index.scss';
+
+import Layout from 'components/Layout'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+
+      <Layout />
+
+   </React.StrictMode>,
+   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
