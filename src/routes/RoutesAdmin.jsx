ef3eaf7b8592e8ea9bed736e4415cbf7342111b4
tahn-0102 <ongthoxunu@gmail.com>
2022-04-04 02:33:01 +0700

    admin login with styled component
    https://dev.to/koolkishan/create-responsive-glassmorphism-login-form-using-react-and-styled-components-5ga1

--- src/routes/RoutesAdmin.jsx
+++ src/routes/RoutesAdmin.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Admin from '../pages/Admin/index.jsx'
+import Login from '../pages/Admin/Login.jsx'
 
 const RoutesAdmin = () => {
    return (
       <Switch>
+         <Route exact path='/admin/login' component={Login} />
          <Route exact path='/admin/dashboard' component={Admin} />
+         <Redirect to="/admin/login" />
       </Switch>
    )
 }
