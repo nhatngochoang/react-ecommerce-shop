ab4b54bc4478a28457473252db5bb65d100ef74c
tahn-0102 <ongthoxunu@gmail.com>
2022-04-07 01:32:55 +0700

    create order

--- src/pages/Admin/pages/Dashboard.jsx
+++ src/pages/Admin/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Chart from 'react-apexcharts'
 import { Link } from 'react-router-dom'
 import statusCards from '../components/assets/JsonData/status-card-data.json'
+import Badge from '../components/badge/index.jsx'
 import StatusCard from '../components/status-card/index.jsx'
 import Table from '../components/table/index.jsx'
 
@@ -84,6 +85,76 @@ const renderCusomerBody = (item, index) => (
    </tr>
 )
 
+const latestOrders = {
+   header: [
+      "order id",
+      "user",
+      "total price",
+      "date",
+      "status"
+   ],
+   body: [
+      {
+         id: "#OD1711",
+         user: "john doe",
+         date: "17 Jun 2021",
+         price: "$900",
+         status: "shipping"
+      },
+      {
+         id: "#OD1712",
+         user: "frank iva",
+         date: "1 Jun 2021",
+         price: "$400",
+         status: "paid"
+      },
+      {
+         id: "#OD1713",
+         user: "anthony baker",
+         date: "27 Jun 2021",
+         price: "$200",
+         status: "pending"
+      },
+      {
+         id: "#OD1712",
+         user: "frank iva",
+         date: "1 Jun 2021",
+         price: "$400",
+         status: "paid"
+      },
+      {
+         id: "#OD1713",
+         user: "anthony baker",
+         date: "27 Jun 2021",
+         price: "$200",
+         status: "refund"
+      }
+   ]
+}
+
+const orderStatus = {
+   "shipping": "primary",
+   "pending": "warning",
+   "paid": "success",
+   "refund": "danger"
+}
+
+const renderOrderHead = (item, index) => (
+   <th key={index}>{item}</th>
+)
+
+const renderOrderBody = (item, index) => (
+   <tr key={index}>
+      <td>{item.id}</td>
+      <td>{item.user}</td>
+      <td>{item.price}</td>
+      <td>{item.date}</td>
+      <td>
+         <Badge type={orderStatus[item.status]} content={item.status} />
+      </td>
+   </tr>
+)
+
 export default function Dashboard() {
    return (
       <div>
@@ -128,7 +199,25 @@ export default function Dashboard() {
                      />
                   </div>
                   <div className="card__footer">
-                     <Link to='/admin/dashboard'>view all</Link>
+                     <Link to='/admin/customers'>view all</Link>
+                  </div>
+               </div>
+            </div>
+            <div className="col-8">
+               <div className="card">
+                  <div className="card__header">
+                     <h3>latest orders</h3>
+                  </div>
+                  <div className="card__body">
+                     <Table
+                        headData={latestOrders.header}
+                        renderHead={(item, index) => renderOrderHead(item, index)}
+                        bodyData={latestOrders.body}
+                        renderBody={(item, index) => renderOrderBody(item, index)}
+                     />
+                  </div>
+                  <div className="card__footer">
+                     <Link to='/admin/orders'>view all</Link>
                   </div>
                </div>
             </div>
