dc60cc5bb017616f3a3b1ec8188e1827610a28f8
tahn-0102 <ongthoxunu@gmail.com>
2022-04-06 20:23:57 +0700

    create fake data + css

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Layout from './components/layout/Layout.jsx'
+import './components/assets/css/index.css'
+import './components/assets/css/theme.css'
 
 export default function Admin() {
    return (
