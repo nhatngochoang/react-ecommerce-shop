cf486032a13ae7047251e84083e9373d47a63c0a
tahn-0102 <ongthoxunu@gmail.com>
2022-04-06 23:08:58 +0700

    contentData + renderItems

--- src/pages/Admin/components/layout/Layout.jsx
+++ src/pages/Admin/components/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import RoutesAdmin from '../../../../routes/RoutesAdmin.jsx'
 import Sidebar from '../sidebar/index.jsx'
+import Topnav from '../topnav/index.jsx'
 import './layout.css'
 
 const adminAccessToken = localStorage.getItem('accessToken')
@@ -19,6 +20,7 @@ export default function Layout() {
                <div>
                   <Sidebar {...props} />
                   <div className="layout__content">
+                     <Topnav />
                      <div className="layout__content-main">
                         <RoutesAdmin />
                      </div>
