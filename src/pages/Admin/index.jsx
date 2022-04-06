6cd20e26f8c8a1ce28eae98ca3f9481c22962285
tahn-0102 <ongthoxunu@gmail.com>
2022-04-07 01:27:20 +0700

    status card + chart + table

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from './components/layout/Layout.jsx'
 import './components/assets/css/index.css'
 import './components/assets/css/theme.css'
+import './components/assets/css/grid.css'
 
 export default function Admin() {
    return (
--- src/pages/Admin/pages/Dashboard.jsx
+++ src/pages/Admin/pages/Dashboard.jsx
@@ -1,7 +1,138 @@
 import React from 'react'
+import Chart from 'react-apexcharts'
+import { Link } from 'react-router-dom'
+import statusCards from '../components/assets/JsonData/status-card-data.json'
+import StatusCard from '../components/status-card/index.jsx'
+import Table from '../components/table/index.jsx'
+
+const chartOptions = {
+   series: [{
+      name: 'Online Customers',
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60]
+   }, {
+      name: 'Store Customers',
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
+   }],
+   options: {
+      color: ['#6ab04c', '#2980b9'],
+      chart: {
+         background: 'transparent'
+      },
+      dataLabels: {
+         enabled: false
+      },
+      stroke: {
+         curve: 'smooth'
+      },
+      xaxis: {
+         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+      },
+      legend: {
+         position: 'top'
+      },
+      grid: {
+         show: false
+      }
+   }
+}
+
+
+const topCustomers = {
+   head: [
+      'user',
+      'total orders',
+      'total spending'
+   ],
+   body: [
+      {
+         "username": "john doe",
+         "order": "490",
+         "price": "$15,870"
+      },
+      {
+         "username": "frank iva",
+         "order": "250",
+         "price": "$12,251"
+      },
+      {
+         "username": "anthony baker",
+         "order": "120",
+         "price": "$10,840"
+      },
+      {
+         "username": "frank iva",
+         "order": "110",
+         "price": "$9,251"
+      },
+      {
+         "username": "anthony baker",
+         "order": "80",
+         "price": "$8,840"
+      }
+   ]
+}
+
+const renderCusomerHead = (item, index) => (
+   <th key={index}>{item}</th>
+)
+
+const renderCusomerBody = (item, index) => (
+   <tr key={index}>
+      <td>{item.username}</td>
+      <td>{item.order}</td>
+      <td>{item.price}</td>
+   </tr>
+)
 
 export default function Dashboard() {
    return (
-      <div>Dashboard</div>
+      <div>
+         <h2 className="page-header">Dashboard</h2>
+         <div className="row">
+            <div className="col-6">
+               <div className="row">
+                  {statusCards.map((item, idx) => {
+                     return (
+                        <div className="col-6">
+                           <StatusCard
+                              icon={item.icon}
+                              count={item.count}
+                              title={item.title} />
+                        </div>
+                     )
+                  })}
+               </div>
+            </div>
+            <div className="col-6">
+               <div className="card full-height">
+                  {/* chart */}
+                  <Chart
+                     options={chartOptions.options}
+                     series={chartOptions.series}
+                     type='line'
+                     height='100%'
+                  />
+               </div>
+            </div>
+            <div className="col-4">
+               <div className="card">
+                  <div className="card__header">
+                     <h3>top customers</h3>
+                  </div>
+                  <div className="card__body">
+                     <Table
+                        headData={topCustomers.head}
+                        renderHead={(item, index) => renderCusomerHead(item, index)}
+                        bodyData={topCustomers.body}
+                        renderBody={(item, index) => renderCusomerBody(item, index)}
+                     />
+                  </div>
+                  <div className="card__footer">
+                     <Link to='/admin/dashboard'>view all</Link>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
    )
 }
