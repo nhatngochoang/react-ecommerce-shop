5e3cdd9008db1ed59d37ac282082ab476c6457ee
tahn-0102 <ongthoxunu@gmail.com>
2022-04-07 19:35:15 +0700

    route tạm thời

--- src/routes/RoutesAdmin.jsx
+++ src/routes/RoutesAdmin.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
+import Analytics from '../pages/Admin/pages/Analytics.jsx'
+import Blank from '../pages/Admin/pages/Blank.jsx'
 import Customers from '../pages/Admin/pages/Customers.jsx'
 import Dashboard from '../pages/Admin/pages/Dashboard.jsx'
 import Login from '../pages/Admin/pages/Login.jsx'
+import Orders from '../pages/Admin/pages/Orders.jsx'
+import Products from '../pages/Admin/pages/Products.jsx'
 import Register from '../pages/Admin/pages/Register.jsx'
 
 const adminAccessToken = localStorage.getItem('accessToken')
@@ -20,6 +24,13 @@ const RoutesAdmin = () => {
       <Switch>
          <Route exact path='/admin/dashboard' component={Dashboard} />
          <Route exact path='/admin/customers' component={Customers} />
+         <Route exact path='/admin/products' component={Products} />
+         <Route exact path='/admin/orders' component={Orders} />
+         <Route exact path='/admin/analytics' component={Analytics} />
+         <Route exact path='/admin/categories' component={Blank} />
+         <Route exact path='/admin/discount' component={Blank} />
+         <Route exact path='/admin/inventory' component={Blank} />
+         <Route exact path='/admin/settings' component={Blank} />
          <Redirect to="/admin/dashboard" />
       </Switch>
    )
