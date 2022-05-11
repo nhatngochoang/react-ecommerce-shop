2a71dac3908d604f7d57b9664ecfcc04de7472cb
tahn-0102 <ongthoxunu@gmail.com>
2022-05-11 13:52:40 +0700

    modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+{/* 
+using
+
+<button className="button-default" onClick={toggle}>Show Modal</button>
+<Modal
+isShowing={isShowing}
+hide={toggle}
+/> 
+
+*/}
+
+const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
+   <React.Fragment>
+      <div className="modal-overlay" />
+      <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+         <div className="modal">
+            <div className="modal-header">
+               <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+                  <span aria-hidden="true">×</span>
+               </button>
+            </div>
+            <p>
+               Hello, I'm a modal.
+            </p>
+         </div>
+      </div>
+   </React.Fragment>, document.body
+) : null;
+
+export default Modal;
